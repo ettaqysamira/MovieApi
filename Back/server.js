d106dc71d4edd998b21e06a231969d795cceb760
samiraGoMyCode <saraelhilloui12@gmail.com>
2025-02-26 01:17:54 +0000

    favori-with-MongoDB

--- Back/server.js
+++ Back/server.js
@@ -19,7 +19,8 @@ mongoose.connect(MONGO_URI)
 const MovieSchema = new mongoose.Schema({
     title: { type: String, required: true },
     imageUrl: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    MovieIsFavorite: { type: Boolean, default: false }
 })
 const Movie = mongoose.model('Movie', MovieSchema)
 
@@ -46,6 +47,28 @@ app.get('/api/movies', async (req, res) => {
     }
 })
 
+app.put('/api/movies/favorite/:id', async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) return res.status(404).json({ error: "Movie not found" })
+
+        movie.MovieIsFavorite = !movie.MovieIsFavorite
+        await movie.save()
+
+        res.json({ message: 'Favorite updated', movie })
+    } catch (error) {
+        res.json({ error: 'Error update' })
+    }
+})
+
+app.get('/api/movies/favorites', async (req, res) => {
+    try {
+        const favorites = await Movie.find({ MovieIsFavorite: true })
+        res.json(favorites)
+    } catch (error) {
+        res.json({ error: 'Error fetching favorites' })
+    }
+});
 
 
 
